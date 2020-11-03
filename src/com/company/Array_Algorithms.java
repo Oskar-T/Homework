5e93963da73da50c735288b64d50756b1820fa6f
Askar <iceaskar@mail.ru>
2020-11-03 19:51:16 +0200

    asd

--- src/com/company/Array_Algorithms.java
+++ src/com/company/Array_Algorithms.java
@@ -6,112 +6,124 @@ import ibadts.*;
 
 public class Array_Algorithms extends Main{
 
-    static void beginning() {
+    static int sequential_search(int []arr, int key) {
 
-        int []values = {1, 2, 24, 64, 10, 63, 12, 17};
-        final  int key = 12;
-        int index = 0;
-        boolean found = false;
-        while (index < values.length && !found) {
-            if(values[index] == key) {
-                found = true;
-                System.out.println(key);
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println("key found: " + (arr[i] == key));
+            if(arr[i] == key) {
+                return key;
             }
-            index++;
         }
-
-        for(int i = 0; i < values.length; i++) {
-
-            if(values[i] == key) {
-                System.out.println(values[i]);
-            }
-
-        }
-
-        index = 0;
-        while (index < values.length) {
-            if(values[index] == key) {
-                System.out.println(values[index]);
-                break;
-            }
-            index++;
-        }
-
-
-
+        return -1;
     }
 
-    static int binary_search(int[] array, int key) {
+    static int binary_search(int[] arr, int key) {
 
         int first = 0;
-        int last = array.length-1;
+        int last = arr.length-1;
+        int middle = (first+last)/2;
 
         while (first <= last) {
-            int middle = (first+last)/2;
-            if(key < array[middle]) {
-                last = middle - 1;
-                System.out.println(array[middle]);
-            } else if(key > array[middle]) {
+
+            if (key > arr[middle]) {
+                System.out.println("key found: " + (arr[middle] == key));
                 first = middle+1;
-                System.out.println(array[middle]);
-            } else  if(key == array[middle]) {
-                System.out.println(array[middle]);
-                return middle;
+            }  else if (key < arr[middle]) {
+                System.out.println("key found: " + (arr[middle] == key));
+                last = middle - 1;
+            }  else if(arr[middle] == key){
+                System.out.println("key found: " + (arr[middle] == key));
+                break;
             }
+
+            middle = (first + last) / 2;
         }
+        return middle;
 
-
-        return -1;
     }
 
     static void selection_sort(int []arr) {
 
-       for(int i = 0; i < arr.length-1; i++) {
-           int index = i;
-           for(int j = i+1; j < arr.length; j++) {
-               if(arr[j] < arr[index]) {
-                   index = j;
-               }
-           }
-           int smaller_number = arr[index];
-           arr[index] = arr[i];
-           arr[i] = smaller_number;
-       }
+
+        int swap;
+        for(int i = 0; i < arr.length-1; i++) {
+            int index = i;
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[index] > arr[j]) {
+                  index = j;
+                }
+            }
+            swap = arr[index];
+            arr[index] = arr[i];
+            arr[i] = swap;
+
+            for(int k : arr) {
+                System.out.println(k);
+            }
+            System.out.println("");
+        }
+
+
 
     }
 
     static void bubble_sort(int []arr) {
 
         int swap;
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = 0; j < arr.length-1-i; j++) {
-                if (arr[j] > arr[j+1]) {
-                    swap = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = swap;
+        for(int i = 0; i < arr.length-1; i++) {
+            for(int j = 0; j < arr.length-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                  swap = arr[j];
+                  arr[j] = arr[j+1];
+                  arr[j+1] = swap;
                 }
             }
+            for(int k : arr) {
+                System.out.println(k);
+            }
+            System.out.println("");
         }
+
     }
 
+    static void hw1() {
+
+        final int key = 13;
+        int[] array = {-2, 9, 11, 12, 13, 14, 15, 22, 142};
+        System.out.println("Sequential search:  ");
+        sequential_search(array, 15);
+        System.out.println("Binary search  ");
+        binary_search(array, 15);
+
+    }
 
     static void hw2() {
 
+        String key = "Charlie";
         String[] students = {"Alissa", "Ben", "Charlie", "Dianna"};
         String[] grades = {"B", "D", "B", "A"};
-        int grade_num = 0;
-
-        for(int i = 0; i < students.length; i++) {
-            if(students[i].equals("Charlie")) {
-                grade_num = i;
+        int key_index = -1;
+        for (int i = 0; i < students.length; i++) {
+            if(students[i].equals(key)) {
+                key_index = i;
             }
         }
-        System.out.println(grades[grade_num]);
+        System.out.println(grades[key_index]);
 
     }
 
     static void hw3() {
-        int[] data = {-4, 11, 7, -12, 6, 1};
+
+        int[] data = {-4, 11, 7, -21, 6, 1};
+        System.out.println("------------------");
+        selection_sort(data);
+        System.out.println("------------------");
+        bubble_sort(data);
+        System.out.println("------------------");
+
+    }
+
+    static void hw4() {
 
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,12 @@
 package com.company;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] numbers = {7, 9, 11, 2};
-        Array_Algorithms.bubble_sort(numbers);
-        for(int s : numbers) {
-            System.out.println(s);
-        }
-
+       Array_Algorithms.hw4();
 
 
     }
