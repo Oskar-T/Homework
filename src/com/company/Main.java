3d36455830ad89d1a1154a6f47405dd3f2f0dd91
Askar <iceaskar@mail.ru>
2021-01-21 14:34:27 +0200

    20.01.2021

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        TwoDimensionalArrays.hw3();
+        TwoDimensionalArrays.hw2();
 
     }
 }
--- src/com/company/TwoDimensionalArrays.java
+++ src/com/company/TwoDimensionalArrays.java
@@ -8,8 +8,8 @@ public class TwoDimensionalArrays {
         String[][] arr = {{"John", "Birgitte", "Greta"}, {"doctor", "lawyer", "doctor"}, {"US", "GB", "SW"}};
 
         String occupation = "doctor";
-        for (String[] personData: arr) {
-            if(personData[1].equals(occupation)) {
+        for (String[] personData : arr) {
+            if (personData[1].equals(occupation)) {
                 System.out.println(personData[0] + " " + personData[2]);
             }
         }
@@ -20,9 +20,9 @@ public class TwoDimensionalArrays {
 
         final int N = 7;
         int[][] table = new int[N][N];
-        for(int r = 0; r < N; r++) {
+        for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
-                table[r][c] = (r+1)/(c+1);
+                table[r][c] = (r + 1) / (c + 1);
                 System.out.print(table[r][c] + " ");
             }
             System.out.println();
@@ -35,8 +35,8 @@ public class TwoDimensionalArrays {
         final int N = 7;
         int counter = 0;
         int[][] table = new int[N][N];
-        for(int r = 0; r < table.length; r++) {
-            for(int c = 0; c < table.length; c++) {
+        for (int r = 0; r < table.length; r++) {
+            for (int c = 0; c < table.length; c++) {
                 table[r][c] = counter++;
             }
             System.out.println();
@@ -55,7 +55,7 @@ public class TwoDimensionalArrays {
         //    2 + 9 - 1 = 10
         //    10 + 3 = 13
         //    13 + 5 - 7 = 11
-        //    answer 3 == 11
+
 
     }
 
@@ -67,6 +67,15 @@ public class TwoDimensionalArrays {
         arr[3][2] = 7;
         arr[5][3] = 22;
 
+        for(int r = 0; r < 6; r++) {
+
+            for(int c = 0; c < 5; c++) {
+                System.out.print(arr[r][c] +" ");
+            }
+            System.out.println();
+
+        }
+
     }
 
     public static void hw3() {
@@ -76,14 +85,50 @@ public class TwoDimensionalArrays {
 
         String key = "d";
         boolean found = false;
-        for(int r = 0; r < arr.length; r++) {
-            for(int c = 0; c < arr.length; c++) {
-                if(arr[r][c].equals(key)) {
+        for (int r = 0; r < arr.length; r++) {
+            for (int c = 0; c < arr.length; c++) {
+                if (arr[r][c].equals(key)) {
                     System.out.println("row: " + r + " column: " + c);
                 }
             }
         }
     }
+
+    public static void hw4() {
+
+        int[][] arr = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+
+        int sOfRows = 0;
+        int sOfColumns = 0;
+        int sOfAll = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                sOfRows += arr[i][j];
+                sOfColumns += arr[j][i];
+                sOfAll += arr[i][j];
+            }
+            System.out.println("sum of row number " + i + " is: " + sOfRows);
+            System.out.println("sum of column number " + i + " is: " + sOfColumns);
+            sOfRows = 0;
+            sOfColumns = 0;
+        }
+        System.out.println("sum of all elements is: " + sOfAll);
+
+    }
+
+    public static void hw5() {
+
+       // palindrome
+
+    }
+
+    static void hw6() {
+        //idk
+    }
+
 
 
 }
