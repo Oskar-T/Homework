a218a44f1379389f18497377016cad16e49caad1
Askar <iceaskar@mail.ru>
2021-02-04 17:40:03 +0200

    04.02.2021

---
+++ src/com/company/IntStack.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class IntStack {
+
+    IntStack (int capacity){
+
+        this.capacity = capacity;
+        itemsCount = 0;
+        IntStack = new int[capacity];
+
+    }
+
+    private int capacity;
+    private int itemsCount;
+    private int[] IntStack;
+
+    void push(int item) {
+
+        if(isEmpty()) {
+            IntStack[0] = item;
+            itemsCount++;
+        } else if(!isFull() && !isEmpty()) {
+            for(int i = itemsCount; i > 0; i--) {
+                IntStack[i] = IntStack[i-1];
+            }
+            IntStack[0] = item;
+            itemsCount++;
+        } else {
+            System.out.println("It's full");
+        }
+
+    }
+
+    int pop() {
+
+        if(!isEmpty()) {
+            itemsCount--;
+            int item = IntStack[0];
+            for(int i = 1; i <=  itemsCount; i++) {
+                IntStack[i-1] = IntStack[i];
+            }
+            IntStack[itemsCount] = 0;
+            return item;
+        }
+
+        return 0;
+    }
+
+    boolean isEmpty() {
+       return itemsCount == 0;
+    }
+
+    boolean isFull() {
+        return itemsCount >= capacity;
+    }
+
+    public String toString() {
+        return Arrays.toString(IntStack);
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        IntStack intStack = new IntStack(5);
+        intStack.push(1);
+        System.out.println(intStack);
+        intStack.push(2);
+        System.out.println(intStack);
+        intStack.push(3);
+        System.out.println(intStack);
+        intStack.push(4);
+        System.out.println(intStack);
+        intStack.push(5);
+        System.out.println(intStack);
 
-        System.out.println(StackCoSc.hw3());
+        System.out.println(intStack.pop());
+        System.out.println(intStack);
+
+
 
     }
 }
